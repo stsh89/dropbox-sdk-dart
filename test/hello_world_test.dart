a9c9afe673137b47878ed7007eca04bb0db30a9a
Stanislav Shandyga <stanislavshandyga@gmail.com>
2018-12-09 09:54:24 +0200

    Setup initial library structure

---
+++ test/hello_world_test.dart
@@ -0,0 +1,8 @@
+import "package:dropbox_sdk_dart/dropbox_sdk_dart.dart";
+import "package:test/test.dart";
+
+void main() {
+  test("returns the string", () {
+    expect(helloWorld(), equals("Hello World!"));
+  });
+}
