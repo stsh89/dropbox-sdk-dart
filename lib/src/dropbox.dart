5c51735ddfe4c0f15eb7d8f50e4870ff385551ac
Stanislav Shandyga <stanislavshandyga@gmail.com>
2018-12-09 15:41:19 +0200

    Adjust whitespaces

--- lib/src/dropbox.dart
+++ lib/src/dropbox.dart
@@ -5,38 +5,38 @@ import "api_client.dart";
 import "mock_api_client.dart";
 
 class Dropbox {
-	String _accessToken;
-	bool _stubResponses;
-	http.Client _client;
+  String _accessToken;
+  bool _stubResponses;
+  http.Client _client;
 
-	Dropbox(String accessToken, {bool stubResponses: false}) {
-		_accessToken = accessToken;
-		_stubResponses = stubResponses;
+  Dropbox(String accessToken, {bool stubResponses: false}) {
+    _accessToken = accessToken;
+    _stubResponses = stubResponses;
 
-		if (_stubResponses) {
-			_client = new MockApiClient();
-		} else {
-			_client = new ApiClient(_accessToken);
-		}
-	}
+    if (_stubResponses) {
+      _client = new MockApiClient();
+    } else {
+      _client = new ApiClient(_accessToken);
+    }
+  }
 
-	String get accessToken {
-		return _accessToken;
-	}
+  String get accessToken {
+    return _accessToken;
+  }
 
-	Future<dynamic> getCurrentAccount() {
-		return _client
-			.post(_apiUrl(dropboxEndpoints["users"]["getCurrentAccount"]))
-			.then((response) {
-				return {
-					"body": json.decode(response.body),
-					"statusCode": response.statusCode
-				};
-			})
-			.whenComplete(_client.close);
-	}
+  Future<dynamic> getCurrentAccount() {
+    return _client
+      .post(_apiUrl(dropboxEndpoints["users"]["getCurrentAccount"]))
+      .then((response) {
+        return {
+          "body": json.decode(response.body),
+          "statusCode": response.statusCode
+        };
+      })
+      .whenComplete(_client.close);
+  }
 
-	String _apiUrl(String endpoint) {
-		return "$dropboxApiBaseUrl/$dropboxApiVersion/$endpoint";
-	}
+  String _apiUrl(String endpoint) {
+    return "$dropboxApiBaseUrl/$dropboxApiVersion/$endpoint";
+  }
 }
